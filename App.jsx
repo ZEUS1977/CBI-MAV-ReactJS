aeeadbe97effa686979805b5d2ddcf676aceff21
pburagina <pburagina@pburagina-ThinkPad-E560>
2017-10-25 21:42:37 +0200

    react-router not working

--- App.jsx
+++ App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 
 class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World!!!
+            <ul>
+               <li><Link to = "/home">Home</Link></li>
+               <li><Link to = "/about">About</Link></li>
+               <li><Link to = "/contact">Contact</Link></li>
+            </ul>
+                
+           {this.props.children}
          </div>
-      );
+      )
    }
 }
 
 export default App;
+
--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
+import Home from './Home.jsx';
+import Contact from './Contact.jsx';
+import About from './About.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+   <Router history = {browserHistory}>
+      <Route path = "/" component = {App}>
+         <IndexRoute component = {Home} />
+         <Route path = "home" component = {Home} />
+         <Route path = "about" component = {About} />
+         <Route path = "contact" component = {Contact} />
+      </Route>
+   </Router>
+	
+), document.getElementById('app'));
